0304c6cd5eb5aeb81e5805dda2bf38e27ee0d123
honeybadger2788 <noe.carosella@gmail.com>
2021-04-29 21:09:27 -0300

    arreglando scripts

--- public/js/activeLink.js
+++ public/js/activeLink.js
@@ -1,20 +1,10 @@
-/*function activeLink(evnt,tag) {
-    evnt.preventDefault()
-    const activeElement = document.getElementByClassName("active");
-    activeElement.classList.remove("active");
-    document.getElementById(tag).classList.add("active");
-}*/
+window.addEventListener('load',()=>{
+  let url = window.location.pathname
 
-function activeLink(evt) {
-    // Declare all variables
-    var i, links;
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    links = document.getElementsByClassName("link");
+  links = document.getElementsByClassName("link");
     for (i = 0; i < links.length; i++) {
-      links[i].className = links[i].className.replace("active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    evt.currentTarget.className += "active";
+      links[i].classList.remove("active");
   }
+
+  document.getElementById(url).classList.add("active");
+})
--- public/js/readMore.js
+++ public/js/readMore.js
@@ -1,15 +1,15 @@
-function readMore() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
+function readMore(id) {
+    var dots = document.getElementById("dots"+id);
+    var moreText = document.getElementById("more"+id);
+    var btnText = document.getElementById("myBtn"+id);
   
     if (dots.style.display === "none") {
       dots.style.display = "inline";
-      btnText.innerHTML = "Read more";
+      btnText.innerHTML = "Leer más";
       moreText.style.display = "none";
     } else {
       dots.style.display = "none";
-      btnText.innerHTML = "Read less";
+      btnText.innerHTML = "Leer menos";
       moreText.style.display = "inline";
     }
   }
