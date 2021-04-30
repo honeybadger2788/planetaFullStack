d36a5fcde166473864e4912f2fa058050e97e95e
honeybadger2788 <noe.carosella@gmail.com>
2021-04-30 12:36:10 -0300

    incluyendo etiquetas meta

--- public/js/typingEffects.js
+++ public/js/typingEffects.js
@@ -4,7 +4,7 @@ var speed = 50;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("demo").innerHTML += txt[i];
     i++;
     setTimeout(typeWriter, speed);
   }
